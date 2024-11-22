35367fdf09aa5fd4a7040f9ea78a3bcbd04d5e12
Paul <paul.lennon@atu.ie>
2024-11-22 16:09:08 +0000

    Added unit test without a mock

--- src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
@@ -3,52 +3,40 @@ package ie.atu.week8.projectexercise;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.Optional;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
-
+    @InjectMocks
+    private ProductService productService;
     @Mock
     private ProductRepository productRepository;
 
-    @InjectMocks
-    private ProductService productService;
-
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @AfterEach
-    void tearDown() {
     }
 
     @Test
-    void getAllProducts() {
+    void testSaveProductForApplyDiscount()
+    {
+        Product product = new Product(1L, "Laptop", "Expensive", 1500);
+        when(productRepository.save(product)).thenReturn(product);
+        Product expectedProduct = productService.saveProduct(product);
+        assertEquals(1350, expectedProduct.getPrice());
     }
-
     @Test
-    void testGetProductById() {
-        Product product = new Product(1L, "Product1", "Description1", 1L);
-        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
-
-        Optional<Product> result = productService.getProductById(1L);
-        assertTrue(result.isPresent());
-        assertEquals(product, result.get());
-
-    }
-
-    @Test
-    void saveProduct() {
-    }
-
-    @Test
-    void deleteProduct() {
+    void testValidateProductForIllegalArgument(){
+        Product product = new Product(1L, "Laptop", "Expensive", 1500);
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, ()-> productService.saveProduct(product));
+        Product expectedProduct = productService.saveProduct(product);
+        assertEquals("Price cannot be negative", iae.getMessage());
     }
 }
